b94007745f6d07fa89494062ded0595267a0cbfd
nagana12 <nagana12@mail.ru>
2017-04-03 00:45:53 +0300

    release

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,5 @@
 $(document).ready(function() {
-    var
-        screen = 0,
-        container = $('.visible__page'),
-        pages = $('.page'),
-        inscroll = false;
 
-    $('page:first-child').addClass('active');
-    $('body').on('mousewheel', function(event) {
-        var
-            activePage = pages.filter('.active');
-        if (!inscroll) {
-            inscroll = true;
-            if (event.deltaY > 0) {
-                if (activePage.prev().length) {
-                    screen--;
-                }
-            } else {
-                if (activePage.next().length) {
-                    screen++;
-                }
-            }
-        }
-        var
-            position = (-screen * 100) + '%';
-        pages.eq(screen).addClass('active').siblings().removeClass('active');
-        container.css('top', position);
-
-        setTimeout(function() {
-            inscroll = false;
-        }, 500);
-    })
 });
 
 $("form").submit(function() {
